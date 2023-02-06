b1e56fad84e3c742ae43fc0a137587d65a6ba8c4
Programming Pleb <dev@pleb.moe>
2023-02-06 17:11:51 +0800

    Use new update package installation method

--- lib/utils/update/checker.dart
+++ lib/utils/update/checker.dart
@@ -1,64 +1,47 @@
 import 'dart:async';
-
-import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_installer/flutter_app_installer.dart';
 import 'package:new_unikl_link/utils/update/popup.dart';
 import 'package:new_unikl_link/utils/update/updater.dart';
 
 Future<void> mainCheckUpdates(BuildContext context) {
-    Completer<void> c = Completer();
+  Completer<void> c = Completer();
 
-    checkUpdates().then((updateData) {
-      if (!updateData.isLatest) {
-        if (updateData.isDownloaded) {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            content: const Text("An app update is now available!"),
-            action: SnackBarAction(
-                label: "Install",
-                onPressed: () {
-                  showUpdatePopup(context: context, updateData: updateData)
-                      .then((confirmed) {
-                    confirmed ??= false;
-                    if (confirmed) {
-                      if (updateData.payloadFile != null) {
-                        AndroidIntent intent = AndroidIntent(
-                          action: "action_install_package",
-                          data: updateData.payloadFile!.path,
-                        );
-                        intent.launch();
-                      }
-                    }
-                  });
-                }),
-            duration: const Duration(days: 365),
-          ));
-        } else {
-          downloadUpdatePayload(updateData).then((dlData) {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: const Text("An app update is now available!"),
-              action: SnackBarAction(
-                  label: "Install",
-                  onPressed: () {
-                    showUpdatePopup(context: context, updateData: updateData)
-                        .then((confirmed) {
-                      confirmed ??= false;
-                      if (confirmed) {
-                        if (updateData.payloadFile != null) {
-                          AndroidIntent intent = AndroidIntent(
-                            action: "action_install_package",
-                            data: updateData.payloadFile!.path,
-                          );
-                          intent.launch();
-                        }
-                      }
-                    });
-                  }),
-              duration: const Duration(days: 365),
-            ));
-          });
-        }
+  checkUpdates().then((updateData) {
+    if (!updateData.isLatest) {
+      if (updateData.isDownloaded) {
+        updateSnackBar(context, updateData);
+      } else {
+        downloadUpdatePayload(updateData).then((dlData) {
+          updateSnackBar(context, updateData);
+        });
       }
-    });
+    }
+  });
 
-    return c.future;
-  }
+  return c.future;
+}
+
+void updateSnackBar(BuildContext context, UpdateData updateData) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: const Text("An app update is now available!"),
+    action: SnackBarAction(
+        label: "Install",
+        onPressed: () {
+          showUpdatePopup(context: context, updateData: updateData)
+              .then((confirmed) {
+            confirmed ??= false;
+            if (confirmed) {
+              if (updateData.payloadFile != null) {
+                FlutterAppInstaller.installApk(
+                        filePath: updateData.payloadFile!.path)
+                    .then((value) => updateSnackBar(context, updateData));
+              }
+            } else {
+              updateSnackBar(context, updateData);
+            }
+          });
+        }),
+    duration: const Duration(days: 365),
+  ));
+}
