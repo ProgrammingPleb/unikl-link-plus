0df1f00169bb9607ef613bba3f5b394b98a06d8b
Programming Pleb <dev@pleb.moe>
2023-03-11 15:09:21 +0800

    Update libraries used by the app Also removes the `flutter_icons` library.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,10 @@ import Foundation
 import dynamic_color
 import mobile_scanner
 import package_info_plus
-import path_provider_macos
+import path_provider_foundation
 import sentry_flutter
-import shared_preferences_macos
+import shared_preferences_foundation
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
@@ -19,4 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
